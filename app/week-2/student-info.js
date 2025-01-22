77ad36c3e08cb9fb9dc353f8eb65789083d9692e
Vanshita Munday <vanshita.munday@edu.sait.ca>
2025-01-22 13:12:50 -0700

    Add week-2 page and StudentInfo component; update root page.

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h2>Vanshita</h2>
+      <p>
+        <Link href="https://github.com/vanshitamunday">
+          Visit My GitHub Repository
+        </Link>
+      </p>
+    </div>
+  );
+}
